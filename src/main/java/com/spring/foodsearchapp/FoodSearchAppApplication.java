b35d9c3cd57f01013ddcffd7b159eeae6f13f533
Grace5252 <gracecappella5252@gmail.com>
2023-04-02 17:02:18 -0400

    Added Property Source to read the application.properties file

--- src/main/java/com/spring/foodsearchapp/FoodSearchAppApplication.java
+++ src/main/java/com/spring/foodsearchapp/FoodSearchAppApplication.java
@@ -2,8 +2,10 @@ package com.spring.foodsearchapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
+@PropertySource(value = "classpath:application.properties", ignoreResourceNotFound = true)
 public class FoodSearchAppApplication {
 
     public static void main(String[] args) {
