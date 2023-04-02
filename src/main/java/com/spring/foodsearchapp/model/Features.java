1fde81ebc1387755a075bcd4c802c3bd71b1fa56
Grace5252 <gracecappella5252@gmail.com>
2023-04-02 18:16:24 -0400

    Revert "Revert "Added getters and setters to model""
    This reverts commit ee1a228e588438f5992f40e9ba73e862a558c7fe.

--- src/main/java/com/spring/foodsearchapp/model/Features.java
+++ src/main/java/com/spring/foodsearchapp/model/Features.java
@@ -5,4 +5,12 @@ import lombok.Data;
 @Data
 public class Features {
     private Object properties;
+
+	public Object getProperties() {
+		return properties;
+	}
+
+	public void setProperties(Object properties) {
+		this.properties = properties;
+	}
 }
